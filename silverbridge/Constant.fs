de805401e5c2d12a608882c59fd3ba476c293f49
Lester Covey <me@lestercovey.ml>
2022-04-23 17:14:46 +0300

    Working auth scheme

---
+++ silverbridge/Constant.fs
@@ -0,0 +1,15 @@
+﻿(***************************
+COPYRIGHT LESTER COVEY (me@lestercovey.ml),
+2021
+
+***************************)
+module Constant
+type Strings =
+    static member Welcome = "Welcome to Silverbridge\n"
+    static member KeyPrompt = "Enter key: "
+    static member EchoCommand = "echo"
+type Endpoint(cmd: string, pass: string) =
+    member private this.Head = "https://api.aydar.media/silverbridge.php?command="
+    member private this.Pass = "&password="
+    member public this.construct = this.Head + cmd + this.Pass + pass
+    new(cmd, pass) = Endpoint(cmd, pass)
--- silverbridge/Program.fs
+++ silverbridge/Program.fs
@@ -4,15 +4,28 @@ COPYRIGHT LESTER COVEY (me@lestercovey.ml),
 
 ***************************)
 open System
+open Constant
+open System.Net
+open System.IO
 
 let setColor color =
     Console.ForegroundColor <- color
 
 [<EntryPoint>]
 let main argv = 
+    setColor ConsoleColor.White;
+    printf "%s" Strings.Welcome;
+    printf "%s" Strings.KeyPrompt;
+    let key = Console.ReadLine();
+    // Making request
+    use s = WebRequest.Create(Endpoint(Strings.EchoCommand, key).construct).GetResponse().GetResponseStream();
+    use sr = new StreamReader(s)
+    let rs = sr.ReadToEnd()
+    printf "%A" rs;
+    exit 0
     while true do 
         let s = Console.ReadLine();
         printf "%s" s;
-
+        
         ()
     0

3c29b847abf5aa7caa685af7cf2ba3a335b497f3
Lester Covey <me@lestercovey.ml>
2022-04-23 15:55:19 +0300

    Initial commit

---
+++ silverbridge/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace silverbridge
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("silverbridge")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Copyright Lester Covey, 2022")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ silverbridge/Program.fs
@@ -0,0 +1,18 @@
+﻿(***************************
+COPYRIGHT LESTER COVEY (me@lestercovey.ml),
+2021
+
+***************************)
+open System
+
+let setColor color =
+    Console.ForegroundColor <- color
+
+[<EntryPoint>]
+let main argv = 
+    while true do 
+        let s = Console.ReadLine();
+        printf "%s" s;
+
+        ()
+    0
